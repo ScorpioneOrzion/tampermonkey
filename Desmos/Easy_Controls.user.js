426c6b1763798fb61d495176b7895095bf8d4583
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-08-15 15:26:02 +0200

    fixed BigIntValue's

--- Desmos/Easy_Controls.user.js
+++ Desmos/Easy_Controls.user.js
@@ -27,11 +27,11 @@ window.addEventListener("keydown", event => {
       if (controlExpression.text.includes("^{")) continue
       const text = controlExpression.text.split(" | ")
       let value = getValue(text[0])
-      const keySub = isNaN(text[1]) ? getValue(text[1]) : BigInt(text[1])
-      const keyAdd = isNaN(text[2]) ? getValue(text[2]) : BigInt(text[2])
-      const step = isNaN(text[3]) ? getValue(text[3]) : BigInt(text[3])
-      const min = isNaN(text[4]) ? getValue(text[4]) : BigInt(text[4])
-      const max = isNaN(text[5]) ? getValue(text[5]) : BigInt(text[5])
+      const keySub = getVar(text, 1)
+      const keyAdd = getVar(text, 2)
+      const step = getVar(text, 3)
+      const min = getVar(text, 4)
+      const max = getVar(text, 5)
       if (event.keyCode == keySub) {
         value = getValue(text[0]) - step
         if (value < min) value += max - min + step
@@ -46,17 +46,21 @@ window.addEventListener("keydown", event => {
     }
   }
 
+  function getVar(text, index) {
+    return isNaN(text[index]) ? getValue(text[index]) : BigInt((text[index] + "").split(".")[0])
+  }
+
   function setExpression(id, value, variable) {
     window.Calc.setExpression({ id, latex: `${variable}=${value + ""}` })
   }
 
   function getValue(variable) {
-    return BigInt(window.Calc.expressionAnalysis[findId(variable)].evaluation.value)
+    return BigInt((window.Calc.expressionAnalysis[findId(variable)].evaluation.value + "").split(".")[0])
   }
 
   function findId(variable) {
     let list = window.Calc.getExpressions().filter(a => a.type == "expression").filter(a => a.latex.includes(variable + "="))
     if (list.length !== 1) return null
-    return BigInt(list[0].id)
+    return BigInt((list[0].id + "").split(".")[0])
   }
 })
