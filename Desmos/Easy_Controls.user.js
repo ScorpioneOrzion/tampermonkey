dde0ab8cf484e9cb6a84c0fdbd2f34706bd24bf9
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-08-15 10:58:22 +0200

    Changed Values to Bigint

--- Desmos/Easy_Controls.user.js
+++ Desmos/Easy_Controls.user.js
@@ -20,29 +20,28 @@ window.addEventListener("keydown", event => {
     if (folderExpression.title !== "controls") continue
     if (folderExpression.hidden === true) continue
     const id = folderExpression.id
-    console.log(id)
     for (const controlExpression of expressions) {
       if (controlExpression.type !== "text") continue
       if (controlExpression.folderId !== id) continue
       if (controlExpression.hidden === true) continue
       const text = controlExpression.text.split(" | ")
-      let value = Number(getValue(text[0]))
+      let value = BigInt(getValue(text[0]))
       if (event.keyCode == text[1]) {
-        value = Number(getValue(text[0])) - Number(text[3])
-        if (value < Number(text[4])) value += Number(text[5]) - Number(text[4])
-        if (value > Number(text[5])) value -= Number(text[5]) - Number(text[4])
-        setExpression(Number(findId(text[0])), value, text[0])
+        value = BigInt(getValue(text[0])) - BigInt(text[3])
+        if (value < BigInt(text[4])) value += BigInt(text[5]) - BigInt(text[4])
+        if (value > BigInt(text[5])) value -= BigInt(text[5]) - BigInt(text[4])
+        setExpression(BigInt(findId(text[0])), value, text[0])
       } else if (event.keyCode == text[2]) {
-        value = Number(getValue(text[0])) + Number(text[3])
-        if (value < Number(text[4])) value += Number(text[5]) - Number(text[4])
-        if (value > Number(text[5])) value -= Number(text[5]) - Number(text[4])
-        setExpression(Number(findId(text[0])), value, text[0])
+        value = BigInt(getValue(text[0])) + BigInt(text[3])
+        if (value < BigInt(text[4])) value += BigInt(text[5]) - BigInt(text[4])
+        if (value > BigInt(text[5])) value -= BigInt(text[5]) - BigInt(text[4])
+        setExpression(BigInt(findId(text[0])), value, text[0])
       }
     }
   }
 
   function setExpression(id, value, variable) {
-    window.Calc.setExpression({ id, latex: `${variable}=${value}` })
+    window.Calc.setExpression({ id, latex: `${variable}=${value + ""}` })
   }
 
   function getValue(variable) {
@@ -52,6 +51,6 @@ window.addEventListener("keydown", event => {
   function findId(variable) {
     let list = window.Calc.getExpressions().filter(a => a.type == "expression").filter(a => a.latex.includes(variable + "="))
     if (list.length !== 1) return null
-    return Number(list[0].id)
+    return BigInt(list[0].id)
   }
 })
