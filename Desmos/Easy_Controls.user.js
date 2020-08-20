ad25340bc4e0f066fe4935196cd422c39047fbfd
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-08-20 13:12:50 +0200

    version update

--- Desmos/Easy_Controls.user.js
+++ Desmos/Easy_Controls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Easy controls
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      2.0
 // @description  Easy setting controls for desmos games
 // @author       Tijmentij
 // @include      https://www.desmos.com/calculator/*
