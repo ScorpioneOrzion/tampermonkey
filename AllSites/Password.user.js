e7bb898b25d27f258e554ef919ea7d7ef7135d42
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-08-25 12:24:00 +0200

    added password generator

---
+++ AllSites/Password.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Password
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Password generator
+// @match        *://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/ScorpioneOrzion/Tampermonkey/master/AllSites/Password.user.js
+// @downloadURL  https://raw.githubusercontent.com/ScorpioneOrzion/Tampermonkey/master/AllSites/Password.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  if (window.createPassword === undefined) {
+    window.createPassword = (len) => {
+      const chars = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
+      return [...Array(len)].map(() => chars[Math.floor(64 * Math.random())]).join("")
+    }
+  }
+})();
