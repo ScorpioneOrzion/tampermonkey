f10a5f424569ed70c33d3ff1fc978b45756b236b
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2023-06-04 20:26:25 +0200

    Update custom.js

--- infinitechess/custom.js
+++ infinitechess/custom.js
@@ -1,1 +1,28 @@
+// ==UserScript==
+// @name         InfinityChess Custom
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  custom infiniteChess
+// @author       ScorpioneOrzion
+// @match        https://www.infinitechess.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=infinitechess.org
+// @grant        none
+// @updateURL    https://github.com/ScorpioneOrzion/tampermonkey/edit/master/infinitechess/custom.js
+// @downloadURL  https://github.com/ScorpioneOrzion/tampermonkey/edit/master/infinitechess/custom.js
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    legalmoves.queens = function () {
+      return {
+        individual: [
+          [-2, 1], [-1, 2], [1, 2], [2, 1],
+          [-2, -1], [-1, -2], [1, -2], [2, -1]
+        ],
+        horizontal: [-Infinity, Infinity],
+        vertical: [-Infinity, Infinity],
+        diagonalUp: [-Infinity, Infinity], // These represent the x limit of the piece sliding diagonally
+        diagonalDown: [-Infinity, Infinity]
+      }
+    }
+})();
