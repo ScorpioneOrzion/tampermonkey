561dbe3bf13f3199d43eaba16cfe22727c0a8f8c
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2023-06-04 20:36:20 +0200

    Update custom.user.js

--- infinitechess/custom.user.js
+++ infinitechess/custom.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         InfinityChess Custom
-// @namespace    http://tampermonkey.net/
+// @namespace    http://github.com/ScorpioneOrzion/tampermonkey
 // @version      0.1
 // @description  custom infiniteChess
 // @author       ScorpioneOrzion
 // @match        https://www.infinitechess.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=infinitechess.org
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/ScorpioneOrzion/tampermonkey/edit/master/infinitechess/custom.user.js
-// @downloadURL  https://raw.githubusercontent.com/ScorpioneOrzion/tampermonkey/edit/master/infinitechess/custom.user.js
+// @updateURL    https://raw.githubusercontent.com/ScorpioneOrzion/tampermonkey/master/infinitechess/custom.user.js
+// @downloadURL  https://raw.githubusercontent.com/ScorpioneOrzion/tampermonkey/master/infinitechess/custom.user.js
 // ==/UserScript==
 
 (function() {
